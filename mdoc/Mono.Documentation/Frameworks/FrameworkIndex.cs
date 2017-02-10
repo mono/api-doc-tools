20596e5174e129d3f45133a363407d7adbf112ac
Joel Martinez <joelmartinez@gmail.com>
2017-02-10 15:18:07 -0500

    Refactored framework index list into a separate class

---
+++ mdoc/Mono.Documentation/Frameworks/FrameworkIndex.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using Mono.Cecil;
+
+namespace Mono.Documentation
+{
+	public class FrameworkTypeEntry
+	{
+		List<string> members = new List<string> ();
+
+		public string Name { get; set; }
+
+		public IList<string> Members
+		{
+			get
+			{
+				return this.members;
+			}
+		}
+
+		public void ProcessMember (IEnumerable<string> signatures) {
+
+		}
+	}
+
+	public class FrameworkEntry
+	{
+		List<FrameworkTypeEntry> types = new List<FrameworkTypeEntry> ();
+
+		public string Name { get; set; }
+
+		public IList<FrameworkTypeEntry> Types {
+			get {
+				return this.types;
+			}
+		}
+
+		public FrameworkTypeEntry ProcessType (TypeDefinition type)
+		{
+			throw new NotImplementedException ();
+		}
+
+}
+
+	public class FrameworkIndex
+	{
+		List<FrameworkEntry> frameworks = new List<FrameworkEntry> ();
+
+		public FrameworkIndex ()
+		{
+		}
+
+		public IList<FrameworkEntry> Frameworks {
+			get {
+				return this.frameworks;
+			}
+		}
+
+		public FrameworkEntry StartProcessingAssembly (AssemblyDefinition assembly)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public void Write ()
+		{
+			throw new NotImplementedException ();
+		}
+
+}
+}
