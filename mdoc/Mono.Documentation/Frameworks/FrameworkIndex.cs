6b00abca5aa0d4375ba28eb933d5ac7ebdc7e49c
Joel Martinez <joelmartinez@gmail.com>
2017-03-03 13:51:13 -0500

    Frameworks Mode for Update Command
    mdoc now supports documenting multiple frameworks. You can use this new functionality by passing the path to a configuration file in the `-frameworks` (or `-fx` for short) parameter.
    
    ```
    mdoc update -o path/to/documentationXML -frameworks path/to/frameworks.xml
    ```
    
    This command will result in one XML file per framework in the `path/to/documentationXML/FrameworksIndex` folder.
    
    ``` xml
    <?xml version="1.0" encoding="utf-8"?>
    <Framework Name="Two">
      <Namespace Name="MyFramework.MyOtherNamespace">
        <Type Name="MyFramework.MyOtherNamespace.MyOtherClass" Id="T:MyFramework.MyOtherNamespace.MyOtherClass">
          <Member Id="M:MyFramework.MyOtherNamespace.MyOtherClass.#ctor" />
          ...
        </Type>
      </Namespace>
    </Framework>
    ```
    
    Configuration File
    
    This is a sample configuration file:
    
    ``` xml
    <?xml version="1.0" encoding="utf-8"?>
    <Frameworks>
      <Framework Name="One" Source="One">
        <assemblySearchPath>dependencies/One</assemblySearchPath>
      </Framework>
      <Framework Name="Two" Source="Two">
        <assemblySearchPath>dependencies/Two</assemblySearchPath>
      </Framework>
    </Frameworks>
    ```
    
    Note that the `assemblySearchPath` is meant to house dependent assemblies that mono.cecil must be able to resolve. You can have as many as you need for each framework if you happen to have them separated into multiple folders. Also the `-L` parameter applies globally to every framework.
    
    Frameworks Config Bootstrap
    
    To simplify the tedium of creating the configuration file, a simple command was added that will use the folder structure to create a basic configuration file:
    
    ``` csharp
    mdoc fx-bootstrap path/to/frameworks
    ```
    
    This will create a file at `path/to/frameworks/frameworks.xml`, using the folders in `path/to/frameworks` as the names/paths
    
    DocId Signatures
    
    You can now pass a flag to add a `DocId` signature to every `Type` and `Member` node.
    
    ```
    mdoc update -o out/path some.dll -use-docid
    ```
    
    _please note_: framework index files will _always_ use the DocId as their identifiers.
    
    Frameworks Index files Grouped by Namespace
    
    Framework index files now have types grouped by namespace. Example:
    
    ``` xml
    <?xml version="1.0" encoding="utf-8"?>
    <Framework Name="Two">
      <Namespace Name="MyFramework.MyOtherNamespace">
        <Type Name="MyFramework.MyOtherNamespace.MyOtherClass" Id="T:MyFramework.MyOtherNamespace.MyOtherClass">
          <Member Id="M:MyFramework.MyOtherNamespace.MyOtherClass.#ctor" />
          ...
        </Type>
      </Namespace>
    </Framework>
    ```
    
    Bug Fixes and Enhancements
    
    - Resolves #13
    - Resolves #27
    - Resolves #31
    - Resolves #33

---
+++ mdoc/Mono.Documentation/Frameworks/FrameworkIndex.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+
+using Mono.Cecil;
+
+namespace Mono.Documentation
+{
+
+	class FrameworkIndex
+	{
+		List<FrameworkEntry> frameworks = new List<FrameworkEntry> ();
+		string path;
+
+		public FrameworkIndex (string pathToFrameworks) 
+		{
+			path = pathToFrameworks;
+		}
+
+		public IList<FrameworkEntry> Frameworks {
+			get {
+				return this.frameworks;
+			}
+		}
+
+		public FrameworkEntry StartProcessingAssembly (AssemblyDefinition assembly) 
+		{
+			if (string.IsNullOrWhiteSpace (this.path))
+				return FrameworkEntry.Empty;
+
+			string assemblyPath = assembly.MainModule.FileName;
+			string relativePath = assemblyPath.Replace (this.path, string.Empty);
+			string shortPath = Path.GetDirectoryName (relativePath);
+			if (shortPath.StartsWith (Path.DirectorySeparatorChar.ToString (), StringComparison.InvariantCultureIgnoreCase))
+				shortPath = shortPath.Substring (1, shortPath.Length - 1);
+			
+
+			var entry = frameworks.FirstOrDefault (f => f.Name.Equals (shortPath));
+			if (entry == null) {
+				entry = new FrameworkEntry { Name = shortPath };
+				frameworks.Add (entry);
+			}
+			return entry;
+		}
+
+		/// <summary>Writes the framework indices to disk.</summary>
+		/// <param name="path">The folder where one file for every FrameworkEntry will be written.</param>
+		public void WriteToDisk (string path) 
+		{
+			if (string.IsNullOrWhiteSpace (this.path))
+				return;
+			
+			string outputPath = Path.Combine (path, "FrameworksIndex");
+			if (!Directory.Exists (outputPath))
+				Directory.CreateDirectory (outputPath);
+			
+			foreach (var fx in this.frameworks) {
+
+				XDocument doc = new XDocument (
+					new XElement("Framework",
+						new XAttribute ("Name", fx.Name),
+			             fx.Types
+			               .GroupBy(t => t.Namespace)
+			               .Select(g => new XElement("Namespace",
+	                           new XAttribute("Name", g.Key),
+	                           g.Select (t => new XElement ("Type",
+							    	new XAttribute ("Name", t.Name),
+                                    new XAttribute("Id", t.Id),
+									t.Members.Select (m =>
+							  			new XElement ("Member",
+								   		new XAttribute ("Id", m)))))))));
+
+				// now save the document
+				string filePath = Path.Combine (outputPath, fx.Name + ".xml");
+
+				if (File.Exists (filePath))
+					File.Delete (filePath);
+
+				var settings = new XmlWriterSettings { Indent = true };
+				using (var writer = XmlWriter.Create (filePath, settings)) {
+					doc.WriteTo (writer);
+				}
+			}
+		}
+	}
+}
---
+++ mdoc/Mono.Documentation/frameworksbootstrapper.cs
@@ -0,0 +1,56 @@
+﻿// Command to preserve member documentation for types that are changing in a subsequent version
+// By Joel Martinez <joel.martinez@xamarin.com
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+using Mono.Options;
+
+namespace Mono.Documentation
+{
+	public class MDocFrameworksBootstrapper : MDocCommand
+	{
+		public override void Run (IEnumerable<string> args)
+		{
+			args = args.Skip (1);
+			if (args.Count () != 1)
+				Error ("Need to supply a single directory, which contain folders that represent frameworks.");
+
+			string frameworkPath = args.Single ();
+			int slashOffset = frameworkPath.EndsWith (Path.DirectorySeparatorChar.ToString (), StringComparison.InvariantCultureIgnoreCase) ? 0 : 1;
+
+			if (!Directory.Exists(frameworkPath)) 
+				Error ($"Path not found: {frameworkPath}");
+
+			var data = Directory.GetDirectories (frameworkPath)
+			                    .Select (d => new {
+									Path = d.Substring (frameworkPath.Length + slashOffset, d.Length - frameworkPath.Length - slashOffset),
+									Name = Path.GetFileName(d)
+								}).ToArray();
+
+			foreach (var d in data)
+				Console.WriteLine (d.Name);
+
+			var doc = new XDocument (
+				new XElement("Frameworks", 
+				             data.Select(d => new XElement(
+					             "Framework",
+					             new XAttribute("Name", d.Name),
+					             new XAttribute("Source", d.Path),
+					             new XElement("assemblySearchPath", Path.Combine("dependencies", d.Name)))))
+			);
+
+			var configPath = Path.Combine (frameworkPath, "frameworks.xml");
+			var settings = new XmlWriterSettings { Indent = true };
+			using (var writer = XmlWriter.Create (configPath, settings)) {
+				doc.WriteTo (writer);
+			}
+
+			Console.WriteLine ($"Framework configuration file written to {configPath}");
+		}
+	}
+}
+
