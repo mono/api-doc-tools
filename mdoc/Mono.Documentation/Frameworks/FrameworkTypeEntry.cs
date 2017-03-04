ee8eddd8ca96bd6e5f0970a0aa101bb52cc82eba
Joel Martinez <joelmartinez@gmail.com>
2017-03-03 21:32:07 -0500

    mdoc: Support for alternate inheritance chains in frameworks.
    If a framework has a different inheritance chain, another `Base/BaseTypeName`
    element will be added with a `FrameworkAlternate` attribute.
    
    Resolves #16

--- mdoc/Mono.Documentation/Frameworks/FrameworkTypeEntry.cs
+++ mdoc/Mono.Documentation/Frameworks/FrameworkTypeEntry.cs
@@ -20,6 +20,7 @@ namespace Mono.Documentation
 		public string Id { get; set; }
 		public string Name { get; set; }
 		public string Namespace { get; set; }
+		public FrameworkEntry Framework { get { return fx; } }
 
 		public ISet<string> Members {
 			get {
