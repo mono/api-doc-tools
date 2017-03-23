9b7a55eec877d3c2d2d08f67f2d9b0b4cbcebb13
Joel Martinez <joelmartinez@gmail.com>
2017-03-23 11:37:36 -0400

    version bump to 5.0.0.10

--- mdoc/Consts.cs
+++ mdoc/Consts.cs
@@ -4,6 +4,6 @@ namespace Mono.Documentation
 	public static class Consts
 	{
 		// this is only a placeholder
-		public static string MonoVersion = "5.0.0.9";
+		public static string MonoVersion = "5.0.0.10";
 	}
 }
