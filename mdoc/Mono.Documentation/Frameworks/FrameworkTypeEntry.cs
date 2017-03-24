2a6454e217ca6cb2fd69c3018c775cb3f114513b
Joel Martinez <joelmartinez@gmail.com>
2017-03-24 19:05:29 -0400

    mdoc: Now caching all framework member lookups. This full resolves #44 as it also uses the correct member formatter for member comparisons

--- mdoc/Mono.Documentation/Frameworks/FrameworkTypeEntry.cs
+++ mdoc/Mono.Documentation/Frameworks/FrameworkTypeEntry.cs
@@ -10,7 +10,7 @@ namespace Mono.Documentation
 		SortedSet<string> members = new SortedSet<string> ();
 		SortedSet<string> memberscsharpsig = new SortedSet<string> ();
 
-		ILMemberFormatter formatter = new ILMemberFormatter ();
+		ILFullMemberFormatter formatter = new ILFullMemberFormatter ();
 
 		FrameworkEntry fx;
 
