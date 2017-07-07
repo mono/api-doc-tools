df0879dd4923fb3a848a1f3c2e721d03a6c44a28
Mike Norman <mike.norman@xamarin.com>
2017-07-07 08:14:58 -0500

    Moved element extraction to static method in CommandUtils

--- tools/DocStat/DocStat/comparereport.cs
+++ tools/DocStat/DocStat/comparereport.cs
@@ -12,7 +12,7 @@ namespace DocStat
         
         public override void Run(IEnumerable<string> args)
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
 
             string updatedDir = "";
 			string omitlist = "";
@@ -25,27 +25,54 @@ namespace DocStat
 															   ref processlist,
 															   ref pattern);
 
-            string oldFiles = "";
+            string oldFilesDir = "";
             bool typeOnly = false;
+            string reportFile = "";
 
+            var options = new OptionSet {
+                {"previous=", (p) => oldFilesDir = p},
+                {"typeonly", (t) => typeOnly = t != null},
+                {"reportfile=", (r) => reportFile = r}
+            };
 
-			if (String.IsNullOrEmpty(oldFiles))
-				throw new ArgumentException("You must supply a parallel directory from which to source new content with '[u|using]'=.");
+            extras = options.Parse(extras);
 
+            CommandUtils.ThrowOnFiniteExtras(extras);
+
+			if (String.IsNullOrEmpty(oldFilesDir))
+				throw new ArgumentException("You must supply a parallel directory from which to source new content with 'previous='.");
+
+            if (String.IsNullOrEmpty(reportFile) || !reportFile.EndsWith(".csv"))
+                throw new ArgumentException("'reportfile=' must be used, and its value must end with '.csv'.");
+
+            string bareReportDir = Path.GetDirectoryName(Path.GetFullPath(reportFile));
+
+            if (!Directory.Exists(bareReportDir))
+                throw new ArgumentException(bareReportDir + " does not exist.");
 
             IEnumerable<string> updated = CommandUtils.GetFileList(processlist, omitlist, updatedDir, pattern);
-			HashSet<string> filesToUseAsReference = new HashSet<string>(CommandUtils.GetFileList("", "", oldFiles, ""));
+			HashSet<string> filesToUseAsReference = new HashSet<string>(CommandUtils.GetFileList("", "", oldFilesDir, ""));
 
 			updated =
 				updated.Where((f) =>
 								 filesToUseAsReference.Contains(ParallelXmlHelper.GetParallelFilePathFor(f,
-																										 oldFiles,
+																										 oldFilesDir,
 																										 updatedDir)));
 
 
+            foreach (string updatedXMLFile in updated)
+            {
+                // For each member in the updated files, report if the old one isn't present
 
-			// For each member in the updated files, report if the old one isn't present
+                string oldXMLFile = ParallelXmlHelper.GetParallelFilePathFor(updatedXMLFile, oldFilesDir, updatedDir);
 
+                bool completelyNew = !File.Exists(oldXMLFile);
+
+                XDocument updatedXDoc = XDocument.Load(updatedXMLFile);
+                XDocument oldXDoc = XDocument.Load(oldXMLFile);
+
+
+            }
 		}
     }
 }
