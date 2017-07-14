b347c0bd1b2fcd28aa89ca2cf54e2cafac4708b2
Mike Norman <mike.norman@xamarin.com>
2017-07-14 11:41:23 -0500

    Fixed bug in comparereport command. Added unit tests.

--- tools/DocStat/DocStat.Tests/EcmaXmlHelperTests.cs
+++ tools/DocStat/DocStat.Tests/EcmaXmlHelperTests.cs
@@ -10,40 +10,61 @@ using DocStat;
 
 namespace DocStat.Tests
 {
-    [TestFixture]
-    public class EcmaXmlHelperTests
-    {
-        [Test]
-        public void NewElementsYieldsAddedElements()
-        {
-            XDocument xmlNew = XDocument.Load("TestData/currentxml/t1.xml");
-            XDocument xmlOld = XDocument.Load("TestData/oldxml/t1.xml");
+	[TestFixture]
+	public class EcmaXmlHelperTests
+	{
+		[Test]
+		public void NewElementsYieldsAddedElements()
+		{
+			XDocument xmlNew = XDocument.Load("TestData/currentxml/t1.xml");
+			XDocument xmlOld = XDocument.Load("TestData/oldxml/t1.xml");
 
-            IEnumerable<XElement> newMembers = EcmaXmlHelper.NewMembers(xmlNew, xmlOld);
+			IEnumerable<XElement> newMembers = EcmaXmlHelper.NewMembers(xmlNew, xmlOld);
 
-            XElement e1 = 
-                xmlNew.Element("Type").Element("Members").Elements()
-                      .FirstOrDefault((XElement arg) => arg.Attribute("MemberName").Value == "WeakDelegate");
+			XElement e1 =
+				xmlNew.Element("Type").Element("Members").Elements()
+					  .FirstOrDefault((XElement arg) => arg.Attribute("MemberName").Value == "WeakDelegate");
 
 			XElement e2 =
 				xmlNew.Element("Type").Element("Members").Elements()
 					  .FirstOrDefault((XElement arg) => arg.Attribute("MemberName").Value == "WeakDataSource");
 
-            Assert.AreEqual(2, newMembers.Count());
-            Assert.True(newMembers.Contains(e1));
-            Assert.True(newMembers.Contains(e2));
-        }
+			Assert.AreEqual(2, newMembers.Count());
+			Assert.True(newMembers.Contains(e1));
+			Assert.True(newMembers.Contains(e2));
+		}
+
+		[Test]
+		public void MembersReturnsMembers()
+		{
+			XDocument xmlOld = XDocument.Load("TestData/oldxml/t1.xml");
+			IEnumerable<XElement> members = EcmaXmlHelper.Members(xmlOld);
+			Assert.AreEqual(13, members.Count());
+
+			members = EcmaXmlHelper.Members(XDocument.Load("TestData/currentxml/t1.xml"));
+
+			Assert.AreEqual(15, members.Count());
+		}
 
         [Test]
-        public void MembersReturnsMembers()
+        public void MembersHandlesEmptyMembersList()
         {
-			XDocument xmlOld = XDocument.Load("TestData/oldxml/t1.xml");
-            IEnumerable<XElement> members = EcmaXmlHelper.Members(xmlOld);
-            Assert.AreEqual(13, members.Count());
+            XDocument x = XDocument.Load("TestData/currentxml/AVAssetImageGeneratorCompletionHandler.xml");
 
-            members = EcmaXmlHelper.Members(XDocument.Load("TestData/currentxml/t1.xml"));
 
-            Assert.AreEqual(15, members.Count());
+                IEnumerable<XElement> elements = EcmaXmlHelper.Members(x);
+                Assert.IsEmpty(elements);
+
         }
-    }
+
+         
+        [Test]
+		public void NewMembersHandlesEmptyMemberList()
+		{
+			XDocument o = XDocument.Load("TestData/oldxml/AVAssetImageGeneratorCompletionHandler.xml");
+			XDocument n = XDocument.Load("TestData/currentxml/AVAssetImageGeneratorCompletionHandler.xml");
+
+            Assert.IsEmpty(EcmaXmlHelper.NewMembers(n, o));
+		}
+	}
 }
