296fb1fd3198b31ebca268df24e79985169bca63
Joel Martinez <joelmartinez@gmail.com>
2017-11-10 11:19:22 -0500

    monodoc: improved LRUCache error condition handling. Closes #95

--- monodoc/Mono.Utilities/LRUCache.cs
+++ monodoc/Mono.Utilities/LRUCache.cs
@@ -1,20 +1,3 @@
-/* 
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 using System;
 using System.Collections.Generic;
 
@@ -47,6 +30,9 @@ namespace Mono.Utilities
         public void Put (TKey key, TValue value)
         {
             if (Get(key) == null) {
+                if (this.openNode.Value == null)
+                    this.openNode.Value = new ListValueEntry<TKey, TValue>(default(TKey), default(TValue));
+
                 this.openNode.Value.ItemKey = key;
                 this.openNode.Value.ItemValue = value;
                 this.list.AddFirst (this.openNode);
