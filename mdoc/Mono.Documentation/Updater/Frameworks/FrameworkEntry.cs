59151b4c4479ed82df4d5fe3ba0c041ea2b5bc30
Joel Martinez <joelmartinez@gmail.com>
2017-11-29 21:12:49 -0500

    WIP: Added type entry to makeparameters, and added index to type entry

--- mdoc/Mono.Documentation/Updater/Frameworks/FrameworkEntry.cs
+++ mdoc/Mono.Documentation/Updater/Frameworks/FrameworkEntry.cs
@@ -6,28 +6,37 @@ using Mono.Cecil.Rocks;
 
 namespace Mono.Documentation.Updater.Frameworks
 {
-	class FrameworkEntry
-	{
-		SortedSet<FrameworkTypeEntry> types = new SortedSet<FrameworkTypeEntry> ();
+    class FrameworkEntry
+    {
+        SortedSet<FrameworkTypeEntry> types = new SortedSet<FrameworkTypeEntry> ();
 
-		List<FrameworkEntry> allframeworks;
+        List<FrameworkEntry> allframeworks;
+        int index = 0;
 
-		public FrameworkEntry (List<FrameworkEntry> frameworks)
-		{
-			allframeworks = frameworks;
-			if (allframeworks == null)
-				allframeworks = new List<FrameworkEntry> (0);
-		}
+        public FrameworkEntry (List<FrameworkEntry> frameworks)
+        {
+            allframeworks = frameworks;
+            if (allframeworks == null)
+                allframeworks = new List<FrameworkEntry> (0);
 
-		public string Name { get; set; }
-		public string Version { get; set; }
-		public string Id { get; set; }
+            index = allframeworks.Count;
+        }
 
-		public IEnumerable<DocumentationImporter> Importers { get; set; }
+        public string Name { get; set; }
+        public string Version { get; set; }
+        public string Id { get; set; }
 
-		public ISet<FrameworkTypeEntry> Types { get { return this.types; } }
+        public IEnumerable<DocumentationImporter> Importers { get; set; }
+
+        public ISet<FrameworkTypeEntry> Types { get { return this.types; } }
         Dictionary<string, FrameworkTypeEntry> typeMap = new Dictionary<string, FrameworkTypeEntry> ();
 
+        public FrameworkTypeEntry FindTypeEntry (FrameworkTypeEntry type) 
+        {
+            return FindTypeEntry (type.Name);    
+        }
+
+        /// <param name="name">The value from <see cref="FrameworkTypeEntry.Name"/>.</param>
         public FrameworkTypeEntry FindTypeEntry (string name) {
             FrameworkTypeEntry entry;
             typeMap.TryGetValue (name, out entry);
