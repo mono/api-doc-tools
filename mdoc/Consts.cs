ecda9993b5490f88a4d3280d1a4970ed64a1d176
Joel Martinez <joelmartinez@gmail.com>
2017-12-22 15:36:11 -0500

    version bump to 5.4

--- mdoc/Consts.cs
+++ mdoc/Consts.cs
@@ -3,7 +3,7 @@ namespace Mono.Documentation
 {
 	public static class Consts
 	{
-		public static string MonoVersion = "5.3.5.3";
+		public static string MonoVersion = "5.4.0.0";
 		public const string DocId = "DocId";
 		public const string VbNet = "VB.NET";
 		public const string DocIdLowCase = "docid";
