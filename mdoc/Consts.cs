d1f6a5b89975b120c406a2975e55a42407a0c012
Joel Martinez <joelmartinez@gmail.com>
2018-01-11 17:04:19 -0500

    version bump to 5.5

--- mdoc/Consts.cs
+++ mdoc/Consts.cs
@@ -3,7 +3,7 @@ namespace Mono.Documentation
 {
 	public static class Consts
 	{
-		public static string MonoVersion = "5.4.1.0";
+		public static string MonoVersion = "5.5.0.0";
 		public const string DocId = "DocId";
 		public const string CppCli = "C++ CLI";
 	    public const string CppCx = "C++ CX";
