445c289dc5f0d59d737c7613b956a66da980548c
Joel Martinez <joelmartinez@gmail.com>
2018-05-03 15:01:44 -0400

    Fixed unit test that failed on mac. Unit test was using FSharpMap, but the list of interfaces being implemented were different on my local install. So to avoid depending on the locally installed SDK, just added a test class that will be consistent to test against.

--- mdoc/mdoc.Test/mdoc.Test.FSharp/Collections.fs
+++ mdoc/mdoc.Test/mdoc.Test.FSharp/Collections.fs
@@ -1,6 +1,18 @@
 ﻿module Collections
 open FSharp.Collections
+open System
+open System.Collections
+open System.Collections.Generic
 
 let f (x:Map<int, int>) = 0
 
 let f2 (x:seq<int>) = 0
+
+type MDocInterface<'key> = interface
+end
+
+type MDocTestMap<'Key, 'Value> = class
+    interface MDocInterface<KeyValuePair<'Key, 'Value>>
+
+
+end
