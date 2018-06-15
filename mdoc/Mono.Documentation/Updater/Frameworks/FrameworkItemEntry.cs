54eaa2a793e7a1cd737c1f4d0b27139a6699db32
Joel Martinez <joelmartinez@gmail.com>
2018-06-15 16:01:19 -0400

    WIP: new API for cached type system

---
+++ mdoc/Mono.Documentation/Updater/Frameworks/FrameworkItemEntry.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mono.Documentation.Updater.Frameworks
+{
+    public class FrameworkItemEntry
+    {
+        List<string> attributes = null;
+        static string[] emptyStringList = new string[0];
+
+        public string Name { get; set; }
+        public string Id { get; set; }
+
+        public FrameworkItemEntry() {}
+        public FrameworkItemEntry (string id) => Id = id;
+        public FrameworkItemEntry (string id, string name) : this (id) => Name = name;
+
+
+        public IEnumerable<string> Attributes {
+            get => attributes == null ? (emptyStringList as IEnumerable<string>): (attributes as IEnumerable<string>);
+        }
+
+        public void AddAttribute (string attributeValue)
+        {
+            if (attributes == null)
+                attributes = new List<string> ();
+
+            attributes.Add (attributeValue);
+        }
+    }
+}
