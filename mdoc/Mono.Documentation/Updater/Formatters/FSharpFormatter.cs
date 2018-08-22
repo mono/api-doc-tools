ce8d121b2f901b5e10611109c962de0458c33fcb
Phillip Carter <phcart@microsoft.com>
2018-08-21 17:12:39 -0700

    Order by name instead of full name

--- mdoc/Mono.Documentation/Updater/Formatters/FSharpFormatter.cs
+++ mdoc/Mono.Documentation/Updater/Formatters/FSharpFormatter.cs
@@ -290,7 +290,7 @@ namespace Mono.Documentation.Updater
                 // There is constructor metadata here for interfaces, but we don't want to actually show it
                 if (!type.IsInterface && !(ctors is null))
                 {
-                    foreach (var ctor in ctors.OrderBy(c => c.FullName))
+                    foreach (var ctor in ctors.OrderBy(c => c.Name))
                     {
                         if (ctor is null) continue;
 
@@ -304,7 +304,7 @@ namespace Mono.Documentation.Updater
 
                 if (!(meths is null))
                 {
-                    foreach (var meth in meths.OrderBy(m => m.FullName))
+                    foreach (var meth in meths.OrderBy(m => m.Name))
                     {
                         if (meth is null) continue;
 
@@ -327,7 +327,7 @@ namespace Mono.Documentation.Updater
         {
             if (type.HasInterfaces)
             {
-                foreach (var interfaceImplementation in type.Interfaces.OrderBy(iface => iface.InterfaceType.FullName))
+                foreach (var interfaceImplementation in type.Interfaces.OrderBy(iface => iface.InterfaceType.Name))
                 {
                     var resolvedInterface = interfaceImplementation.InterfaceType.Resolve();
 
