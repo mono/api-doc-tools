621ed2f43343be18b2424d891bcaf32f170cad81
Phillip Carter <phcart@microsoft.com>
2018-08-23 15:21:44 -0700

    Filter out compiler generated members

---
+++ mdoc/Mono.Documentation/Util/MemberGenUtil.cs
@@ -0,0 +1,18 @@
+﻿using Mono.Cecil;
+using Mono.Documentation;
+using Mono.Documentation.Util;
+using System.Linq;
+
+namespace mdoc.Mono.Documentation.Util
+{
+    public static class MemberGenUtil
+    {
+        public static bool IsUserDefined(this MethodDefinition method)
+            => method.CustomAttributes.Any(a => a.GetDeclaringType() != Consts.CompilerGeneratedAttribute ||
+                                                a.GetDeclaringType() != Consts.CompilationMappingAttribute);
+
+        public static bool IsUserDefined(this PropertyDefinition prop)
+            => prop.CustomAttributes.Any(a => a.GetDeclaringType() != Consts.CompilerGeneratedAttribute ||
+                                              a.GetDeclaringType() != Consts.CompilationMappingAttribute);
+    }
+}
