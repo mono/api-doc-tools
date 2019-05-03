02321c00faf33247468c35951df2813dfd72e1d4
Dave Wilson <cdwilson@windows.microsoft.com>
2019-05-03 14:17:52 -0700

    UWP compiled metadata files use "put_" as the prefix to property setter methods. Enable support to suppress such methods from ecma xml creation.

---
+++ mdoc/mdoc.Test/UWPDocUtilsTests.cs
@@ -0,0 +1,24 @@
+﻿using mdoc.Test.SampleClasses;
+using Mono.Documentation.Updater;
+using NUnit.Framework;
+
+namespace mdoc.Test
+{
+    [TestFixture]
+    public class UWPDocUtilsTests : BasicTests
+    {
+        private const string UWPTestComponentWinMD = "../../../../external/Test/UWPTestComponentCSharp.winmd";
+
+        [Test]
+        public void IsIgnored_PutMethodIsGeneratedByProperty_IsIgnoredTrue()
+        {
+            var classWithProperties = GetType(UWPTestComponentWinMD, "mdoc.Test.UWP.TestComponent.UwpClassWithProperties");
+
+            var method = GetMethod(classWithProperties, i => i.Name == "put_MyReadWriteProperty");
+
+            var isIgnoredPropertyGeneratedMethod = DocUtils.IsIgnored(method);
+
+            Assert.IsTrue(isIgnoredPropertyGeneratedMethod);
+        }
+    }
+}
