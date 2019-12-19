43b970fead8ee4b1bb872c810536c6cd6fd0996d
maopeixia <maopeixia@163.com>
2019-12-19 10:53:40 +0800

    mpx-test

---
+++ mdoc/mdoc.Test/CsharpFormatterTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using mdoc.Test.SampleClasses;
+using Mono.Documentation.Updater;
+
+namespace mdoc.Test
+{
+    [TestFixture]
+    public class CsharpFormatterTests : BasicFormatterTests<CSharpMemberFormatter>
+    {
+        private CSharpMemberFormatter csharpMemberFormatter = new CSharpMemberFormatter();
+        protected override CSharpMemberFormatter formatter => csharpMemberFormatter;
+
+        [Test]
+        public void FuncParams()
+        {
+            var member = GetMethod(typeof(TestClass), m => m.Name == "DoWithNullParams");
+            var sig = formatter.GetDeclaration(member);
+            Assert.AreEqual("public string DoWithNullParams (out string a, object b = default, TestClass c = default);", sig);
+        }
+    }
+}
