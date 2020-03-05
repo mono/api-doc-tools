f867c532572299242d41019dd28b7e6028bcaa84
Joel Martinez <joelmartinez@gmail.com>
2020-03-05 16:37:08 -0500

    [import] fixed issue with xpath

--- mdoc/mdoc.Test/DocUtilsTests.cs
+++ mdoc/mdoc.Test/DocUtilsTests.cs
@@ -65,7 +65,40 @@ namespace mdoc.Test
             XmlDocument incomingDoc = new XmlDocument();
             incomingDoc.LoadXml(xml2);
             DocUtils.ClearNodesIfNotDefault(doc.FirstChild, incomingDoc.FirstChild);
-            Assert.IsTrue(doc.FirstChild.ChildNodes.Count == 3) ;
+            Assert.IsTrue(doc.FirstChild.ChildNodes.Count == 3);
+        }
+
+        [Test]
+        public void TestNodeCleaner2()
+        {
+            string xml = @"<Docs>
+    <summary>To be added.</summary>
+    <param name=""one"" name2=""n2"">To be added.</param>
+<!-- this is an xml comment -->
+    <param name=""two"">written docs</param>
+random text
+<param name=""three"">Written but not provided</param>
+<![CDATA[
+  for some reason
+]]>
+</Docs>"; string xml2 = @"<Docs>
+    <summary>new summary</summary>
+<!-- this is another xml comment -->
+    <param name=""one"" name2=""n2"">To be added.</param>
+    random text
+    <param name=""two"">written docs but changed</param>
+<param name=""three"">Written but and</param>
+<![CDATA[
+  for some reason
+]]>
+</Docs>";
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xml);
+
+            XmlDocument incomingDoc = new XmlDocument();
+            incomingDoc.LoadXml(xml2);
+            DocUtils.ClearNodesIfNotDefault(doc.FirstChild, incomingDoc.FirstChild);
+            Assert.IsTrue(doc.FirstChild.ChildNodes.Count == 0);
         }
     }
 }
