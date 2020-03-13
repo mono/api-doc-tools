749c0dcc8d0f9097815b7aebd6094db458834d09
Ann Meng <anmeng@microsoft.com>
2020-03-13 13:34:47 +0800

    add smoke test case

---
+++ mdoc/Test/DocTest-nestedType-typeForwards.cs
@@ -0,0 +1,15 @@
+#if SECOND || THIRD
+using System.Runtime.CompilerServices;
+[assembly:TypeForwardedToAttribute(typeof(TheNamespace.TheClass))] 
+#endif
+
+namespace TheNamespace
+{
+    #if FIRST
+    public class TheClass
+    {
+        protected class InnerClass
+        {}
+    }
+    #endif
+}
