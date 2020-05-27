1e6bd541c0ae6978b261a65cea5fe94cd3aaf4dc
hiihellox10 <basehello@icloud.com>
2020-05-27 23:52:44 +0800

    Fix VB default value for class parameters bug (#499)
    * Fix VB default value for class parameters bug
    https://ceapex.visualstudio.com/Engineering/_workitems/edit/221125

--- mdoc/Mono.Documentation/Updater/Formatters/VBFullMemberFormatter.cs
+++ mdoc/Mono.Documentation/Updater/Formatters/VBFullMemberFormatter.cs
@@ -583,7 +583,8 @@ namespace Mono.Documentation.Updater
             buf.Append(GetTypeName(parameter.ParameterType, new DynamicParserContext(parameter)));
             if (parameter.HasDefault && parameter.IsOptional && parameter.HasConstant)
             {
-                buf.AppendFormat(" = {0}", MDocUpdater.MakeAttributesValueString(parameter.Constant, parameter.ParameterType));
+                var parameterValue = MDocUpdater.MakeAttributesValueString(parameter.Constant, parameter.ParameterType);
+                buf.AppendFormat(" = {0}", parameterValue == "null" ? "Nothing" : parameterValue);
             }
             return buf;
         }
