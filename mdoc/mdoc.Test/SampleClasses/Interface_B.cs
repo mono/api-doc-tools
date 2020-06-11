02f82884dee2f940a7cf772f60884d0c70b85487
maopeixia <maopeixia@163.com>
2020-06-11 14:42:54 +0800

    [mdoc] come up with test/design for better member implementation detection

---
+++ mdoc/mdoc.Test/SampleClasses/Interface_B.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mdoc.Test2
+{
+   public interface Interface_B
+   {
+        string color { get; set; }
+        int no { get;  }
+        string GetNo();
+        bool IsNum(string no);
+        event EventHandler<EventArgs> ItemChanged;
+
+
+    }
+}
