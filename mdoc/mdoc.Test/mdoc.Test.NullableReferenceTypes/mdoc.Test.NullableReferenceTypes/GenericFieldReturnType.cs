4ff99b531ad26f77de3fc1448ae02352d4c3ecc1
Xiao Luo <basehello@icloud.com>
2021-01-18 14:48:40 +0800

    Add test project of nullable reference types and move the mdoc.Test.Cplusplus project to the mdoc.Test folder.

---
+++ mdoc/mdoc.Test/mdoc.Test.NullableReferenceTypes/mdoc.Test.NullableReferenceTypes/GenericFieldReturnType.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace mdoc.Test.NullableReferenceTypes
+{
+    public class GenericFieldType<T>
+    {
+        public T GenericType;
+    }
+
+    public class GenericFieldTypeOfValueType<T> where T : struct
+    {
+        public T GenericType;
+        public T? NullableGenericType;
+        public ICollection<T> InterfaceOfGenericType;
+        public ICollection<T?> InterfaceOfNullableGenericType;
+        public ICollection<T>? NullableInterfaceOfGenericType;
+        public ICollection<T?>? NullableInterfaceOfNullableGenericType;
+        public Dictionary<Dictionary<T, string>, string> DictionaryOfDictionary;
+        public Dictionary<Dictionary<T?, string>, string> DictionaryOfDictionaryOfNullableGenericTypeKey;
+        public Dictionary<Dictionary<T?, string>, string>? NullableDictionaryOfDictionaryOfNullableGenericTypeKey;
+    }
+
+    public class GenericFieldTypeOfReferenceType<T> where T : class
+    {
+        public T GenericType;
+        public T? NullableGenericType;
+        public ICollection<T> InterfaceOfGenericType;
+        public ICollection<T?> InterfaceOfNullableGenericType;
+        public ICollection<T>? NullableInterfaceOfGenericType;
+        public ICollection<T?>? NullableInterfaceOfNullableGenericType;
+        public Dictionary<Dictionary<T, string>, string> DictionaryOfDictionary;
+        public Dictionary<Dictionary<T?, string>, string> DictionaryOfDictionaryOfNullableGenericTypeKey;
+        public Dictionary<Dictionary<T?, string>, string>? NullableDictionaryOfDictionaryOfNullableGenericTypeKey;
+    }
+}
---
+++ mdoc/mdoc.Test/mdoc.Test.NullableReferenceTypes/mdoc.Test.NullableReferenceTypes/GenericMethodReturnType.cs
@@ -0,0 +1,30 @@
+﻿namespace mdoc.Test.NullableReferenceTypes
+{
+    public class GenericMethodReturnType
+    {
+        public T GenericType<T>()
+        {
+            return default;
+        }
+
+        public T GenericReferenceType<T>() where T : class
+        {
+            return default;
+        }
+
+        public T? GenericNullableReferenceType<T>() where T : class
+        {
+            return default;
+        }
+
+        public T GenericValueType<T>() where T : struct
+        {
+            return default;
+        }
+
+        public T? GenericNullableValueType<T>() where T : struct
+        {
+            return default;
+        }
+    }
+}
