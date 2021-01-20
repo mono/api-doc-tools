8b52f5293ccfad0a4f754c10d27d308e89d901e7
Xiao Luo <basehello@icloud.com>
2021-01-21 05:39:03 +0800

    new dotnet core test project for nullables (#534)
    Also moved the cplusplus project out of root dir

---
+++ mdoc/mdoc.Test/mdoc.Test.NullableReferenceTypes/mdoc.Test.NullableReferenceTypes/Event.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace mdoc.Test.NullableReferenceTypes
+{
+    public class Event
+    {
+        public event EventHandler EventHandler;
+
+        public event EventHandler? NullableEventHandler;
+
+        public event EventHandler<EventArgs> GenericEventHandler;
+
+        public event EventHandler<EventArgs?> GenericEventHandlerOfNullableEventArgs;
+
+        public event EventHandler<EventArgs>? NullableGenericEventHandler;
+
+        public event EventHandler<EventArgs?>? NullableGenericEventHandlerOfNullableEventArgs;
+    }
+}
