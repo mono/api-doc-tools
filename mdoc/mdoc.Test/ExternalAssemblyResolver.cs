37ae438b80fa2b6e0c9518605c452e653a316006
Xiao Luo <basehello@icloud.com>
2021-01-27 17:25:55 +0800

    Fix the Mono.Cecil can't load .NET Core platform assemblies in  WSL, macOS, and Ubuntu OS environment.

---
+++ mdoc/mdoc.Test/ExternalAssemblyResolver.cs
@@ -0,0 +1,25 @@
+﻿using Mono.Cecil;
+using System.IO;
+
+namespace mdoc.Test
+{
+    public class ExternalAssemblyResolver : DefaultAssemblyResolver
+    {
+        private const string ExternalTestAssemblyDirectory = "../../../../external/Test";
+
+        public ExternalAssemblyResolver()
+        {
+            AddExternalSearchDirectory();
+        }
+
+        private void AddExternalSearchDirectory()
+        {
+            AddSearchDirectory(GetExternalAssemblyPath(ExternalTestAssemblyDirectory));
+        }
+
+        private string GetExternalAssemblyPath(string externalTestAssemblyDirectory)
+        {
+            return Path.Combine(Path.GetDirectoryName(this.GetType().Module.Assembly.Location), externalTestAssemblyDirectory);
+        }
+    }
+}
