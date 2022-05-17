4b2e81183f6c6c1f0d9b1893036e10270f706978
v-susu1 <v-susu@microsoft.com>
2022-05-18 00:15:44 +0800

    Update compareXmlFiles.ps1

--- mdoc/compareXmlFiles.ps1
+++ mdoc/compareXmlFiles.ps1
@@ -4,8 +4,8 @@ param (
     [string]$githubOptionsAccountName,
     [string]$githubOptionsAccountEmail,
     [string]$vstsTokenBase64,
-    [bool]$needRunReleaseMdoc,
-    [string]$step
+    [string]$step,
+    [string]$commit1
 )
 
 function Git-Init([string]$githubAccountName, [string]$githubAccountEmail)
@@ -280,7 +280,7 @@ if($step -eq "1"){
 	Write-Host "##vso[task.setvariable variable=commit1;isOutput=true]$commitid1"
 } else { # This part(else) run in Job_2
 	$commitid2 = "abcdefghigklmn"
-	$commitid1 = '$(commit1)'    # commit1 from job_1
+	$commitid1 = $commit1    # commit1 from job_1
 	$shortCommitId1 = $commitid1.Substring(0, 7)
 	$shortCommitId2 = $commitid2.Substring(0, 7)
 	$targetRepoUrl = $params.target_repo.url
