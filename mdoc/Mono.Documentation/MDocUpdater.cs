mono/api-doc-tools

commit eb7fec62c3ec4759ae178bd601564aa4f4d66ed6

Fix the logic for the shouldDelete factor (#628)
* Fix the logic for the shouldDelete factor

* Fix cmd for integration test

* Make duplication a considered factor in the deleting

mdoc/Mono.Documentation/MDocUpdater.cs
@@ -1939,9 +1939,11 @@ namespace Mono.Documentation
                     signature);
 
             // Identify all of the different states that could affect our decision to delete the member
+            bool duplicated = reason.Contains("Duplicate Member");
             bool shouldPreserve = !string.IsNullOrWhiteSpace (PreserveTag);
             bool hasContent = MemberDocsHaveUserContent (member);
-            bool shouldDelete = !shouldPreserve && (delete || !hasContent);
+            //When the member is NOT PRESERVED, the member has NO CONTENT or  is DUPLICATED, then it should be deleted
+            bool shouldDelete = !shouldPreserve && (delete && (!hasContent || duplicated));
 
             bool unifiedRun = HasDroppedNamespace (type);
 

mdoc/Makefile
@@ -751,7 +751,7 @@ check-monodocer-attached-entities:
 
 	# now make sure it will delete a previously run/duplicated attachedproperty/property
 	cp Test/AttachedEventsAndProperties/AttachedPropertyExample.xml Test/en.actual/AttachedEventsAndProperties/
-	$(MONO) $(PROGRAM) update -o Test/en.actual Test/AttachedEventsAndProperties/bin/Release/AttachedEventsAndProperties.dll -lang docid -lang vb.net -lang fsharp -lang javascript -lang c++/cli -lang c++/cx -lang c++/winrt
+	$(MONO) $(PROGRAM) update -o Test/en.actual Test/AttachedEventsAndProperties/bin/Release/AttachedEventsAndProperties.dll --delete -lang docid -lang vb.net -lang fsharp -lang javascript -lang c++/cli -lang c++/cx -lang c++/winrt
 	$(DIFF) Test/en.expected-attached-entities Test/en.actual
 	
 Test/TestClass.dll:
@@ -765,7 +765,7 @@ check-monodocer-operators-work: Test/TestClass.dll
 	rm -Rf Test/en.actual
 	$(MONO) $(PROGRAM) update Test/TestClass.dll -o Test/en.actual
 	cp mdoc.Test/SampleClasses/TestClass-OldOpSig.xml Test/en.actual/mdoc.Test.SampleClasses/TestClass.xml
-	$(MONO) $(PROGRAM) update Test/TestClass.dll -o Test/en.actual
+	$(MONO) $(PROGRAM) update Test/TestClass.dll -o Test/en.actual --delete
 
 .PHONY: check-monodocer-operators
 check-monodocer-operators: check-monodocer-operators-work 

commit a664d8c5c89e6f62f28ecad5bc86377e1b4585ec

[MDocUpdater] Fix usage of the --delete option
Commit eb7fec62c introduced a breaking change that caused doc removal to
be skipped for members that were removed from a type:

    mdoc: Member Removed: File='/Users/peter/source/android-api-docs/docs/Mono.Android/en/Android.Text/AlteredCharSequence.xml'; Signature='System.Collections.IEnumerator IEnumerable.GetEnumerator ();'
    mdoc: Not deleting 'System.Collections.IEnumerator IEnumerable.GetEnumerator ();'; must be enabled with the --delete option

The error message is now misleading in this case.  I am using the
`--delete` parameter when running the tool, but it no longer works for
removed members.

To help address the scenario fixed in eb7fec62c, a new `--retain-eii`
parameter has been added to mdoc.  When this is parameter is set and
explicit interface implementation docs are not empty, they will not be
removed (even when `--delete` is used).

The `--delete` behavior has otherwise been reverted to its previous
state, and the new `--retain-eii` can be set to opt in to this new EII
specific beahvior.

mdoc/Mono.Documentation/MDocUpdater.cs
@@ -34,6 +34,7 @@ namespace Mono.Documentation
         bool isClassicRun;
         bool verbose;
         bool delete;
+        bool retainEII;
         bool show_exceptions;
         bool no_assembly_versions, ignore_missing_types;
         ExceptionLocations? exceptions;
@@ -188,6 +189,9 @@ namespace Mono.Documentation
             { "delete",
                 "Delete removed members from the XML files.",
                 v => delete = v != null },
+            { "retain-eii",
+                "Retain non-empty explicit interface implementation docs in the XML files instead of regenerating them.",
+                v => retainEII = v != null },
             { "exceptions:",
               "Document potential exceptions that members can generate.  {SOURCES} " +
                 "is a comma-separated list of:\n" +
@@ -1592,6 +1596,10 @@ namespace Mono.Documentation
                     if (!no_assembly_versions && UpdateAssemblyVersions (oldmember, type.Module.Assembly, new string[] { GetAssemblyVersion (type.Module.Assembly) }, false))
                         continue;
 
+                    // If a private EII has existing docs we may not want to delete and regenerate them
+                    if (isprivateeii && retainEII && MemberDocsHaveUserContent (oldmember))
+                        continue;
+
                     DeleteMember ("Member Removed", output, oldmember, todelete, type);
                     statisticsCollector.AddMetric(typeEntry.Framework.Name, StatisticsItem.Members, StatisticsMetrics.Removed);
                     continue;
@@ -1939,11 +1947,10 @@ namespace Mono.Documentation
                     signature);
 
             // Identify all of the different states that could affect our decision to delete the member
-            bool duplicated = reason.Contains("Duplicate Member");
             bool shouldPreserve = !string.IsNullOrWhiteSpace (PreserveTag);
+            // For historical consistency, docs with no content will be removed even if the --delete param is not used.
             bool hasContent = MemberDocsHaveUserContent (member);
-            //When the member is NOT PRESERVED, the member has NO CONTENT or  is DUPLICATED, then it should be deleted
-            bool shouldDelete = !shouldPreserve && (delete && (!hasContent || duplicated));
+            bool shouldDelete = !shouldPreserve && (delete || !hasContent);
 
             bool unifiedRun = HasDroppedNamespace (type);
 