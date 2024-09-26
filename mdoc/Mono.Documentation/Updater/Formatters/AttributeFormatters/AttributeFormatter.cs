4c7e8ccc36e2d8175d2e48fe1fc8ff3be716322f
Fuquan Li (INFOSYS LIMITED) <v-fuquanli@microsoft.com>
2024-09-26 15:04:07 +0800

    update

--- mdoc/Mono.Documentation/Updater/Formatters/AttributeFormatters/AttributeFormatter.cs
+++ mdoc/Mono.Documentation/Updater/Formatters/AttributeFormatters/AttributeFormatter.cs
@@ -181,8 +181,7 @@ namespace Mono.Documentation.Updater.Formatters
 		    "System.Runtime.CompilerServices.DynamicAttribute",
 		    // F# compiler attribute
 		    "Microsoft.FSharp.Core.CompilationMapping",
-            "System.Runtime.CompilerServices.NullableContextAttribute",
-            "System.Runtime.CompilerServices.NullableAttribute"
+            "System.Runtime.CompilerServices.NullableContextAttribute"
         };
     }
 }
